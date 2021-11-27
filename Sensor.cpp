4577b08f05af92b9fd6815d5679de131cc5b759d
cbraissant <chrisbraissant@gmail.com>
2021-11-27 18:05:00 +0100

    Refactor to pass the adc as a parameter

--- Sensor.cpp
+++ Sensor.cpp
@@ -2,70 +2,34 @@
 
 #include <Adafruit_ADS1X15.h>
 
-Sensor::Sensor(int adc_channel, int adc_multiplier) {
-    this->adc_multiplier = adc_multiplier;
+Sensor::Sensor(Adafruit_ADS1115 &ads1115, int adc_channel, int adc_resolution) {
+    this->ads1115 = &ads1115;
     this->adc_channel = adc_channel;
-    this->sensor_is_calibrated = false;
-    init();
+    this->adc_resolution = adc_resolution;
+    this->is_calibrated = false;
 }
 
 bool Sensor::isCalibrated() {
-    return sensor_is_calibrated;
+    return is_calibrated;
 }
 
-void Sensor::adjust_gain() {
-    switch (this->adc_multiplier) {
-        case (0):
-            ads1115.setGain(GAIN_TWOTHIRDS);
-            this->sensor_mv_per_bit = 0.1875;
-            break;
-        case (1):
-            ads1115.setGain(GAIN_ONE);
-            this->sensor_mv_per_bit = 0.125;
-            break;
-        case (2):
-            ads1115.setGain(GAIN_TWO);
-            this->sensor_mv_per_bit = 0.0625;
-            break;
-        case (4):
-            ads1115.setGain(GAIN_FOUR);
-            this->sensor_mv_per_bit = 0.03125;
-            break;
-        case (8):
-            ads1115.setGain(GAIN_EIGHT);
-            this->sensor_mv_per_bit = 0.015625;
-            break;
-        case (16):
-            ads1115.setGain(GAIN_SIXTEEN);
-            this->sensor_mv_per_bit = 0.0078125;
-            break;
-        default:
-            ads1115.setGain(GAIN_TWO);
-            this->sensor_mv_per_bit = 0.0625;
-    }
+int16_t Sensor::read_value() {
+    return ads1115->readADC_SingleEnded(adc_channel);
 }
 
-void Sensor::init() {
-    this->adjust_gain();
-    ads1115.begin();
+float Sensor::read_mV() {
+    return this->read_value() * this->adc_resolution;
 }
 
-int16_t Sensor::get_adc_value() {
-    return this->ads1115.readADC_SingleEnded(this->adc_channel);
-}
 
-float Sensor::get_mV() {
-    return this->get_adc_value() * this->sensor_mv_per_bit;
-}
+// float Sensor::get_PO2() {
+//     if (this->is_calibrated) {
+//         return this->read_mV() / this->sensor_gain;
+//     } else {
+//         return 0.0;
+//     }
+// }
 
-float Sensor::get_PO2() {
-    if (this->sensor_is_calibrated) {
-        return this->get_mV() / this->sensor_gain;
-    } else {
-        return 0.0;
-    }
-}
-
-void Sensor::calibrate(int calibration_FO2) {
-    this->sensor_is_calibrated = true;
-}
+// void Sensor::calibrate(int calibration_FO2) {
+//     this->is_calibrated = true;
+// }
--- Sensor.h
+++ Sensor.h
@@ -3,32 +3,20 @@
 
 #include <Adafruit_ADS1X15.h>
 #include <Arduino.h>
-// #include <RunningAverage.h>
 
 class Sensor {
-   private:
-    int adc_multiplier;
-    int adc_channel;
-    int calibration_FO2;
-    int sensor_gain;
-    float sensor_mv_per_bit;
-    bool sensor_is_calibrated;
-    // RunningAverage sensor_ra;
-    Adafruit_ADS1115 ads1115;
-
    public:
-    Sensor(int adc_channel, int adc_multiplier);
-
-    void init();
-    void adjust_gain();
-    void calibrate(int calibration_FO2);
-
+    Sensor(Adafruit_ADS1115 &ads1115, int adc_channel, int adc_resolution);
+    int16_t read_value();
+    float read_mV();
+    float read_pO2();
     bool isCalibrated();
 
-    // returns the different values of the sensor
-    int16_t get_adc_value();
-    float get_mV();
-    float get_PO2();
+   private:
+    Adafruit_ADS1115 *ads1115;
+    int adc_channel;
+    int adc_resolution;
+    int is_calibrated;
 };
 
 #endif
