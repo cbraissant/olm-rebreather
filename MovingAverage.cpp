d170078dcec6cdc3f3f54373c61c0fd53c38f55b
cbraissant <chrisbraissant@gmail.com>
2021-11-28 12:23:31 +0100

    create the moving average

---
+++ MovingAverage.cpp
@@ -0,0 +1,30 @@
+#include "MovingAverage.h"
+
+MovingAverage::MovingAverage(int max_length) {
+    readings = new int[max_length];
+    reset();
+}
+
+void MovingAverage::writeValue(int new_value) {
+    // update the array of values and its index
+    readings[index_next] = new_value;
+    index_next = ++index_next % max_length;
+
+    if (length++ < max_length) {
+        // update the sum
+        sum = sum + new_value;
+    } else {
+        // if the array is full remove the oldest value
+        sum = sum - readings[index_next] + new_value;
+    }
+}
+
+int MovingAverage::readAverage() {
+    return sum / length;
+}
+
+void MovingAverage::reset() {
+    index_next = 0;
+    length = 0;
+    sum = 0;
+}
---
+++ MovingAverage.h
@@ -0,0 +1,21 @@
+#ifndef MOVINGAVERAGE_H
+#define MOVINGAVERAGE_H
+
+#include <Arduino.h>
+
+class MovingAverage {
+   public:
+    MovingAverage(int max_length);
+    void writeValue(int new_value);
+    int readAverage();
+    void reset();
+
+   private:
+    int *readings;   // pointer to the array of values
+    int index_next;  // index to the next value in the array
+    int length;      // current length of the array
+    int max_length;  // maximum length of the array
+    int sum;         // sum of all the values in the array
+};
+
+#endif
