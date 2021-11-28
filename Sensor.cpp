67fd8c9a391bd703f1b2b1edc1d14175cb3f5820
cbraissant <chrisbraissant@gmail.com>
2021-11-28 19:41:51 +0100

    add moving average

--- Sensor.cpp
+++ Sensor.cpp
@@ -2,34 +2,46 @@
 
 #include <Adafruit_ADS1X15.h>
 
-Sensor::Sensor(Adafruit_ADS1115 &ads1115, int adc_channel, int adc_resolution) {
+#include "MovingAverage.h"
+
+Sensor::Sensor(Adafruit_ADS1115 &ads1115, int adc_channel, float adc_resolution) {
     this->ads1115 = &ads1115;
     this->adc_channel = adc_channel;
     this->adc_resolution = adc_resolution;
     this->is_calibrated = false;
+    this->moving_average = new MovingAverage(20);
 }
 
-bool Sensor::isCalibrated() {
+bool Sensor::readCalibration() {
     return is_calibrated;
 }
 
-int16_t Sensor::read_value() {
+int16_t Sensor::readValue() {
     return ads1115->readADC_SingleEnded(adc_channel);
 }
 
-float Sensor::read_mV() {
-    return this->read_value() * this->adc_resolution;
+float Sensor::readVoltage() {
+    return readAverage() * adc_resolution;
 }
 
+float Sensor::readAverage() {
+    return moving_average->readAverage();
+}
 
-// float Sensor::get_PO2() {
-//     if (this->is_calibrated) {
-//         return this->read_mV() / this->sensor_gain;
-//     } else {
-//         return 0.0;
-//     }
-// }
+void Sensor::updateAverage() {
+    moving_average->writeValue(readValue());
+}
 
-// void Sensor::calibrate(int calibration_FO2) {
-//     this->is_calibrated = true;
-// }
+void Sensor::calibrate(float calibration_fO2) {
+    this->calibration_value = readAverage();
+    this->calibrated_gain = calibration_fO2 / calibration_value;
+    this->is_calibrated = true;
+}
+
+float Sensor::readPressure() {
+    if (this->is_calibrated) {
+        return readAverage() * calibrated_gain;
+    } else {
+        return 0.0;
+    }
+}
--- Sensor.h
+++ Sensor.h
@@ -4,19 +4,27 @@
 #include <Adafruit_ADS1X15.h>
 #include <Arduino.h>
 
+#include "MovingAverage.h"
+
 class Sensor {
    public:
-    Sensor(Adafruit_ADS1115 &ads1115, int adc_channel, int adc_resolution);
-    int16_t read_value();
-    float read_mV();
-    float read_pO2();
-    bool isCalibrated();
+    Sensor(Adafruit_ADS1115 &ads1115, int adc_channel, float adc_resolution);
+    int16_t readValue();
+    float readAverage();
+    float readVoltage();
+    float readPressure();
+    void calibrate(float calibration_fO2);
+    bool readCalibration();
+    void updateAverage();
+    int16_t calibration_value;
 
    private:
     Adafruit_ADS1115 *ads1115;
+    MovingAverage *moving_average;
     int adc_channel;
-    int adc_resolution;
+    float adc_resolution;
     int is_calibrated;
+    float calibrated_gain;
 };
 
 #endif
